0263c83b580048edaa904c621f530560efd64e06
Oleksiy Pogiba <opogiba@e1r8p8.unit.ua>
2017-03-18 00:52:10 +0200

    ds

--- printf/utilites_for_double/ft_round.c
+++ printf/utilites_for_double/ft_round.c
@@ -32,28 +32,43 @@ long double	ft_r_nbr(long double nbr)
 		return (ft_floor(nbr));
 }
 
+char		*ft_round(long double nbr, t_arg *func, char *str)
+{
+	int			i;
+	int			acc;
+	int 		j;
+
+	j = 0;
+	acc = func->acc;
+	while (acc > 0)
+	{
+		nbr *= 10;
+		if (acc == 1)
+			nbr = ft_r_nbr(nbr);
+		i = (int) nbr;
+		str[j] = (char)(i + '0');
+		nbr = nbr - i;
+		acc--;
+		j++;
+	}
+	return (str);
+}
 //char		*ft_round(long double nbr, t_arg *func, char *str)
 //{
 //	int			i;
-//	int			acc;
-//	int 		j;
+//	int			p;
 //
-//	j = 0;
-//	acc = func->acc;
-//	while (acc > 0)
+//	p = func->acc;
+//	while (p > 0)
 //	{
 //		nbr *= 10;
-//		if (acc == 1)
+//		if (p == 1)
 //			nbr = ft_r_nbr(nbr);
-//		i = (int) nbr;
-//		str[j] = (char)(i + '0');
+//		i = (int)nbr;
+//		*str = i + '0';
 //		nbr = nbr - i;
-//		acc--;
-//		j++;
-////		*str = i + '0';
-////		nbr = nbr - i;
-////		acc--;
-////		str++;
+//		p--;
+//		str++;
 //	}
 //	return (str);
 //}
@@ -93,25 +108,7 @@ long double	ft_r_nbr(long double nbr)
 ////	nbr_to_str(nbr_l, 10, &str);
 //	return (str);
 //}
-char		*ft_round(long double nbr, t_arg *func, char *str)
-{
-	int			i;
-	int			p;
 
-	p = func->acc;
-	while (p > 0)
-	{
-		nbr *= 10;
-		if (p == 1)
-			nbr = ft_r_nbr(nbr);
-		i = (int)nbr;
-		*str = i + '0';
-		nbr = nbr - i;
-		p--;
-		str++;
-	}
-	return (str);
-}
 void		ft_round_d(long double nbr, t_arg *func, char *str)
 {
 	int			p;
