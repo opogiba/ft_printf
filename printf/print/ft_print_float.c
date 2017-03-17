c3f43081190a1ac23142bc86d2a3bdfecf3edd97
Oleksiy Pogiba <opogiba@e1r8p8.unit.ua>
2017-03-18 01:09:00 +0200

    ds

--- printf/print/ft_print_float.c
+++ printf/print/ft_print_float.c
@@ -12,26 +12,6 @@
 
 #include "../../ft_printf.h"
 
-void	nbr_to_str(unsigned long long value, unsigned long long base,
-				   char **str)
-{
-	char	c;
-
-	if (value >= base)
-	{
-		nbr_to_str(value / base, base, str);
-		nbr_to_str(value % base, base, str);
-	}
-	else
-	{
-
-		c = value + '0';
-		**str = c;
-		(*str)++;
-	}
-	**str = '\0';
-}
-
 char 	*ft_print_float(char *string, t_arg *func, long double nbr)
 {
 	long long	exponenta;
@@ -56,31 +36,3 @@ char 	*ft_print_float(char *string, t_arg *func, long double nbr)
 	free(string2);
 	return (string);
 }
-
-//char	*ft_print_float(char *string, t_arg *func, long double nbr)
-//{
-//	long long	exponenta;
-//	char *finish;
-//	char *tmp;
-//
-//	nbr = ft_parsing_znak_double(nbr, func);
-//	if (func->acc_ex == 1 && func->acc == 0)
-//		nbr = ft_r_nbr(nbr);
-//	exponenta = (long long)nbr;
-//	string = ft_itoa_base(exponenta, 10);
-//	finish = (char *) malloc(sizeof(char) * func->acc + 1);
-//	finish[func->acc] = '\0';
-//	if(func->acc > 0 && func->acc <= 15)
-//		finish = ft_round_d(nbr - (long double)exponenta, func, finish);
-//	else if(func->acc == 0 || func->acc > 15)
-//		finish = ft_round(nbr - (long double)exponenta, func, finish);
-//	if((int)ft_strlen(string2) != func->acc)
-//		ft_check_len((int)ft_strlen(string2), func->acc, string2);
-
-//	string = ft_dot_to_float(string, func);
-//	tmp = finish;
-//	finish = ft_strjoin(string, finish);
-//	free(string);
-//	free(tmp);
-//	return (finish);
-//}
