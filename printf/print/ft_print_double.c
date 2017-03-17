4773b072c412a7e3152ff6568fb0db29347949fd
Oleksiy Pogiba <opogiba@e1r8p8.unit.ua>
2017-03-18 01:31:18 +0200

    ds

--- printf/print/ft_print_double.c
+++ printf/print/ft_print_double.c
@@ -12,18 +12,10 @@
 
 #include "../../ft_printf.h"
 
-char	*ft_get_double(char *string, t_arg *func, va_list args)
+char	*ft_get_double(char *string, t_arg *func, long double nbr)
 {
-	long double	nbr;
-	int			minus;
-
-	if (func->doub_len == 1)
-		nbr = (long double)va_arg(args, long double);
-	else
-		nbr = (double)va_arg(args, double);
 	if ((string = ft_check_inf(nbr, func)) != NULL)
 		return (string);
-	minus = nbr < 0 ? -1 : 1;
 	if (func->chr == 'f' || func->chr == 'F')
 		string = ft_print_float(string, func, nbr);
 	else if (func->chr == 'e' || func->chr == 'E')
@@ -32,27 +24,30 @@ char	*ft_get_double(char *string, t_arg *func, va_list args)
 		string = ft_print_float_or_exponent(string, func, nbr);
 	else if (func->chr == 'a' || func->chr == 'A')
 		string = ft_print_float_or_exponent(string, func, nbr);
-	string = ft_width_int(func, string, minus, (int)ft_strlen(string));
 	return (string);
 }
 
 void	ft_print_double(t_format *form, t_arg *func, va_list args)
 {
 	char *string;
+	long double	nbr;
+	int			minus;
 
-	string = NULL;
 	if (func->acc_ex != 1)
 	{
 		func->acc = 6;
 		func->acc_ex = 1;
 	}
-	string = ft_get_double(string, func, args);
+	if (func->doub_len == 1)
+		nbr = (long double)va_arg(args, long double);
+	else
+		nbr = (double)va_arg(args, double);
+	minus = nbr < 0 ? -1 : 1;
+	string = NULL;
+	string = ft_get_double(string, func, nbr);
 	if (func->f_apos == 1)
 		string = ft_put_apostrophe(string, func);
-
-
-
-
+	string = ft_width_int(func, string, minus, (int)ft_strlen(string));
 	ft_print(string, form);
 	free(string);
 }
