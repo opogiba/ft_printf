5a08d016cc280b2c686bbc3886136eb914cdba72
Oleksiy Pogiba <opogiba@e1r8p8.unit.ua>
2017-03-17 20:46:47 +0200

    float3

--- printf/print/ft_print_float_or_exponent.c
+++ printf/print/ft_print_float_or_exponent.c
@@ -9,12 +9,19 @@ char 	*ft_print_float_or_exponent(char *string, t_arg *func, long double nbr)
 	len = (int)ft_strlen(ft_itoa((int)nbr));
 	func->acc = func->acc - len;
 	func->acc = func->acc >= 0 ? func->acc : 0;
+	if(acc == len)
+	{
+		string = ft_print_float(string, func, nbr);
+		string =ft_del_zero(string, func);
+		return (string);
+	}
 	if(len > acc)
 	{
 		func->base = 11;
 		string = ft_print_exponent_double(string, func, nbr);
 		return (string);
 	}
+
 
 
 
