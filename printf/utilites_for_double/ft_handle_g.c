e2acf10c0aa991fe151cd267bd39d24314e59d2b
Oleksiy Pogiba <opogiba@e1r8p8.unit.ua>
2017-03-18 03:38:07 +0200

    try g

--- printf/utilites_for_double/ft_handle_g.c
+++ printf/utilites_for_double/ft_handle_g.c
@@ -22,17 +22,17 @@ char 	*ft_del_zero(char *string, t_arg *func)
 
 char 	*ft_check_g(int exponenta, char *string, t_arg *func, long double nbr)
 {
-	 int len;
-	 int acc;
+//	 int len;
+//	 int acc;
+//
+//
+//	acc = func->acc;
+//
+//	len = (int)ft_strlen(ft_itoa_base((long int)nbr, 10));
+//	func->acc = func->acc - len;
+//	func->acc = func->acc >= 0 ? func->acc : 0;
 
-
-	acc = func->acc;
-
-	len = (int)ft_strlen(ft_itoa_base((long int)nbr, 10));
-	func->acc = func->acc - len;
-	func->acc = func->acc >= 0 ? func->acc : 0;
-
-	if(((exponenta < -4 ) || (exponenta >= func->acc && func->acc_ex == 1)) || len < acc )
+	if(((exponenta < -4 ) || (exponenta >= func->acc && func->acc_ex == 1)))
 	{
 		string = ft_do_exponent_string(nbr, func, exponenta);
 		return (string);
