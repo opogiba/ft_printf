22e541da7f09c568823e31f4e2cbd5c66b877394
Oleksiy Pogiba <opogiba@e1r8p8.unit.ua>
2017-03-18 03:54:26 +0200

    try g

--- printf/print/ft_print_float_or_exponent.c
+++ printf/print/ft_print_float_or_exponent.c
@@ -10,9 +10,9 @@ char 	*ft_print_float_or_exponent(char *string, t_arg *func, long double nbr)
 	string = ft_print_exponent_double(string, func, nbr);
 	if(string == NULL)
 	{
-		len = (int)ft_strlen(ft_itoa((int)nbr));
-		func->acc = func->acc - len;
-		func->acc = func->acc >= 0 ? func->acc : 0;
+//		len = (int)ft_strlen(ft_itoa((int)nbr));
+//		func->acc = func->acc - len;
+//		func->acc = func->acc >= 0 ? func->acc : 0;
 
 		string = ft_print_float(string, func, nbr);
 		string =ft_del_zero(string, func);
