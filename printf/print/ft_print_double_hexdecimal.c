77d4c494ad96bc8d4b02f72a6669b4969e5b8f4c
Oleksiy Pogiba <opogiba@e1r8p8.unit.ua>
2017-03-19 16:04:28 +0200

    chandge x

--- printf/print/ft_print_double_hexdecimal.c
+++ printf/print/ft_print_double_hexdecimal.c
@@ -18,8 +18,9 @@ char	*ft_do_first_hex_string(char *final, t_arg *func, double long nbr)
 	final = ft_join_char(final, 0, '0');
 	if (func->chr == 'A')
 	{
-		final = ft_join_char(final, 0, 'X');
+//		final = ft_join_char(final, 0, 'X');
 		func->chr = 'X';
+		func->f_hash = 1;
 	}
 	else
 		final = ft_join_char(final, 0, 'x');
